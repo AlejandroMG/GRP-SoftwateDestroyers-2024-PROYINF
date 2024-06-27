4818bd45d530b6cddd9cbcc6a5246d75f4cc3f10
static-reset <163859298+static-reset@users.noreply.github.com>
2024-06-26 22:24:27 -0400

    Update view.py

--- dicomViewer/view.py
+++ dicomViewer/view.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter import ttk
 from PIL import Image, ImageTk
+from tkinter import Scrollbar
 
 
 class DicomView(tk.Tk):
@@ -11,7 +12,7 @@ class DicomView(tk.Tk):
         self.title("DICOM Viewer")
         self.geometry("1000x700")
         self.configure(bg="#303030")
-        self.folder_path = None  # Añadir atributo folder_path
+        self.folder_path = None
         self._create_widgets()
 
     def show_info(self, text):
@@ -22,22 +23,24 @@ class DicomView(tk.Tk):
         filter_frame = tk.Frame(self, bg="#303030")
         filter_frame.pack(side=tk.TOP, fill=tk.X, pady=10, padx=10)
 
-        
-
         # Barra de búsqueda
         tk.Label(filter_frame, text="Barra de búsqueda", bg="#303030", fg="#ffffff", font=("Helvetica", 12)).pack(side=tk.LEFT, padx=5)
-        search_entry = tk.Entry(filter_frame, bg="#ffffff", fg="#000000", font=("Helvetica", 12), bd=0)
-        search_entry.pack(side=tk.LEFT, padx=5, pady=5, fill=tk.X, expand=True)
+        self.search_entry = tk.Entry(filter_frame, bg="#ffffff", fg="#000000", font=("Helvetica", 12), bd=0)
+        self.search_entry.pack(side=tk.LEFT, padx=5, pady=5, fill=tk.X, expand=True)
 
         # Filtro de fecha
         tk.Label(filter_frame, text="Filtro Fecha", bg="#303030", fg="#ffffff", font=("Helvetica", 12)).pack(side=tk.LEFT, padx=5)
-        date_entry = tk.Entry(filter_frame, bg="#ffffff", fg="#000000", font=("Helvetica", 12), bd=0)
-        date_entry.pack(side=tk.LEFT, padx=5, pady=5)
+        self.date_entry = tk.Entry(filter_frame, bg="#ffffff", fg="#000000", font=("Helvetica", 12), bd=0)
+        self.date_entry.pack(side=tk.LEFT, padx=5, pady=5)
 
         # ID Paciente
         tk.Label(filter_frame, text="Id Paciente", bg="#303030", fg="#ffffff", font=("Helvetica", 12)).pack(side=tk.LEFT, padx=5)
-        id_entry = tk.Entry(filter_frame, bg="#ffffff", fg="#000000", font=("Helvetica", 12), bd=0)
-        id_entry.pack(side=tk.LEFT, padx=5, pady=5)
+        self.id_entry = tk.Entry(filter_frame, bg="#ffffff", fg="#000000", font=("Helvetica", 12), bd=0)
+        self.id_entry.pack(side=tk.LEFT, padx=5, pady=5)
+
+        # Botón para aplicar filtros
+        self.apply_filter_button = tk.Button(filter_frame, text="Aplicar Filtros", command=self.controller.apply_filters, bg="#007bff", fg="#ffffff", font=("Helvetica", 14))
+        self.apply_filter_button.pack(side=tk.LEFT, padx=5, pady=5)
 
         # Frame central para la tabla y previsualización
         middle_frame = tk.Frame(self, bg="#303030")
@@ -50,7 +53,12 @@ class DicomView(tk.Tk):
         self.tree.heading("ID Paciente", text="Id_paciente")
         self.tree.heading("Fecha", text="Fecha")
         self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        
+
+        # Scrollbar vertical
+        tree_scrollbar_y = tk.Scrollbar(middle_frame, orient=tk.VERTICAL, command=self.tree.yview)
+        tree_scrollbar_y.pack(side=tk.LEFT, fill=tk.Y)
+        self.tree.configure(yscrollcommand=tree_scrollbar_y.set)
+
         # Vincular la selección del Treeview al controlador
         self.tree.bind("<ButtonRelease-1>", self.controller.select_dicom_file)
 
@@ -58,13 +66,11 @@ class DicomView(tk.Tk):
         preview_frame = tk.Frame(middle_frame, bg="#303030")
         preview_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=10)
 
-
         # Botón para ver imagen en una nueva ventana
         self.view_image_button = tk.Button(preview_frame, text="Ver Imagen", command=self.controller.show_full_image, bg="#007bff", fg="#ffffff", font=("Helvetica", 14))
         self.view_image_button.pack(pady=10)
 
         # Botón para mostrar la cabecera DICOM
-        
         self.show_header_button = tk.Button(preview_frame, text="Mostrar Cabecera DICOM", command=self.controller.show_dicom_header, bg="#007bff", fg="#ffffff", font=("Helvetica", 14))
         self.show_header_button.pack(side=tk.BOTTOM, pady=10)
 
@@ -81,20 +87,16 @@ class DicomView(tk.Tk):
         self.open_button = tk.Button(self, text="Abrir carpeta DICOM", command=self.controller.open_dicom_folder, bg="#007bff", fg="#ffffff", font=("Helvetica", 14))
         self.open_button.pack(side=tk.BOTTOM, pady=10)
 
-        
-
     def show_image(self, image):
         tk_image = ImageTk.PhotoImage(image)
         self.image_label.config(image=tk_image)
         self.image_label.image = tk_image
-
 
     def show_dicom_header_window(self, dicom_header):
         if dicom_header:
             # Crear una nueva ventana toplevel para la cabecera DICOM
             header_window = tk.Toplevel(self)
             header_window.title("Cabecera DICOM")
-
             header_window.geometry("640x480")
 
             # Crear un frame para la cabecera DICOM
@@ -114,5 +116,7 @@ class DicomView(tk.Tk):
 
     def open_folder_dialog(self):
         folder_path = filedialog.askdirectory(title="Selecciona una carpeta que contenga archivos DICOM")
-        self.folder_path = folder_path  # Actualizar folder_path
+        self.folder_path = folder_path
         return folder_path
+    
+     
